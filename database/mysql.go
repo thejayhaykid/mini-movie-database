6fa8adac7eed835208ad6dd1afff212663388853
Jake J. Hayes <jhayes@hmbnet.com>
2018-10-26 15:35:39 -0400

    Progress on mySQL section

--- database/mysql.go
+++ database/mysql.go
@@ -1,1 +1,19 @@
 package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// ConnectDB connects to the mySQL database and returns that connection.
+//  Connection string format is user:password@tcp(localhost:5555)/dbname?charset=utf8
+func ConnectDB(conStr string) *sql.DB {
+	db, err := sql.Open("mysql", conStr)
+	if err != nil {
+		fmt.Println("ERROR connecting to database.")
+		panic(err)
+	}
+	return db
+}
--- models/dump-object.go
+++ models/dump-object.go
@@ -1,1 +1,13 @@
 package models
+
+// DumpMovie struct is the structure of a movie from a JSON file dumped
+//  by tMDB daily with an added processed field to tell if the row in mySQL
+//  database.
+type DumpMovie struct {
+	adult      bool
+	id         int32
+	title      string
+	popularity float64
+	video      bool
+	processed  bool
+}
